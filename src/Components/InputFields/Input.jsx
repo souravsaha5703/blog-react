ea790dd5dff3881275a15181e7b760d68a99e3ec
Sourav Saha <saha83787@gmail.com>
2024-03-03 21:40:10 +0530

    Input component created

---
+++ src/Components/InputFields/Input.jsx
@@ -0,0 +1,26 @@
+import React, { useId } from 'react'
+
+function Input({
+    label,
+    type,
+    className = "",
+    ...props
+}) {
+    const id = useId();
+    return (
+        <div className='w-full'>
+            {label && <label
+                className='inline-block my-2 text-lg font-noto font-semibold text-left text-slate-950'
+                htmlFor={id}>
+                {label}
+            </label>}
+            <input type={type}
+            className={`outline-none w-full h-12 px-3 py-1 border-2 focus:border-slate-950 font-noto font-medium text-lg text-slate-950 rounded-md duration-200 ease-linear placeholder:font-medium bg-slate-200 placeholder:text-black placeholder:text-base capitalize ${className}`}
+            id={id}
+            {...props}
+             />
+        </div>
+    )
+}
+
+export default Input
