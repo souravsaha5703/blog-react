037c41fe5dbc5ac66549c3dc4af68c6327db1168
Sourav Saha <saha83787@gmail.com>
2024-03-12 21:29:24 +0530

    responsive applied

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BsFilterRight } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
 import { NavLink } from 'react-router-dom'
 
 function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const handleToggle = () => {
+        setToggleMenu(!toggleMenu);
+    }
     return (
-        <div className='w-full py-5 px-10 flex items-center justify-between fixed backdrop-blur-sm z-20'>
+        <div className={` ${toggleMenu ? 'w-full py-5 px-10 flex flex-col items-center justify-between fixed bg-slate-200 z-20' : 'w-full py-5 px-10 flex items-center justify-between fixed backdrop-blur-sm z-20 shadow-md'}`}>
             <h1 className='font-oswald font-semibold text-3xl'>Logo</h1>
-            <div className='flex px-4 items-center justify-center gap-2'>
+            <button className={`px-1 py-1 rounded-md border-2 border-gray-300 cursor-pointer ${toggleMenu ? 'hidden' : 'hidden max-[900px]:block'}`} onClick={handleToggle}><BsFilterRight className='text-4xl' /></button>
+            <div className={`flex px-4 items-center justify-center gap-2 ${toggleMenu ? 'flex-col mt-5' : 'max-[900px]:hidden'}`}>
                 <NavLink to={"/"} className={({ isActive }) => `text-xl font-noto font-medium cursor-pointer text-black hover:bg-black hover:text-white duration-100 ease-in-out rounded-full px-4 transition-colors py-2 capitalize text-center ${isActive ? "bg-black text-white" : "text-black"}`}>Home</NavLink>
-                <NavLink to={"/explore"} className={({ isActive }) => `text-xl font-noto font-medium cursor-pointer text-black hover:bg-black hover:text-white duration-100 ease-in-out rounded-full px-4 transition-colors py-2 capitalize text-center ${isActive ? "bg-black text-white" : "text-black"}`}>Explore</NavLink>
-                <p className='text-xl font-noto font-medium cursor-pointer text-black hover:bg-black hover:text-white duration-100 ease-in-out rounded-full px-4 transition-colors py-2 capitalize text-center'>Behind the scenes</p>
-                <p className='text-xl font-noto font-medium cursor-pointer text-black hover:bg-black hover:text-white duration-100 ease-in-out rounded-full px-4 transition-colors py-2 capitalize text-center'>Talk to Us</p>
+                <NavLink to={"/explore"} className={({ isActive }) => `text-xl font-noto font-medium cursor-pointer text-black hover:bg-black hover:text-white duration-100 ease-in-out rounded-full px-4 transition-colors py-2 capitalize text-center ${isActive ? "bg-black text-white" : "text-black"}`}>Behind the scenes</NavLink>
+                <NavLink className='text-xl font-noto font-medium cursor-pointer text-black hover:bg-black hover:text-white duration-100 ease-in-out rounded-full px-4 transition-colors py-2 capitalize text-center'>Talk to Us</NavLink>
             </div>
-            <div className='flex px-4 items-center justify-center gap-4'>
+            <div className={`flex px-4 items-center justify-center gap-2 ${toggleMenu ? 'flex-col mt-5' : 'max-[900px]:hidden'}`}>
                 <NavLink to={"/login"} className='text-xl font-noto font-medium cursor-pointer px-5 py-2 border border-black rounded-full relative overflow-hidden after:content-[""] after:absolute after:h-full after:w-full after:left-0 after:bottom-[-100%] after:bg-black after:rounded-full after:duration-150 after:ease-in-out after:hover:bottom-0 after:hover:rounded-none group'><p className='text-black z-50 relative duration-150 ease-in-out group-hover:text-white'>Login</p></NavLink>
                 <NavLink to={"/register"} className='text-xl font-noto font-medium cursor-pointer px-5 py-2 border border-black rounded-full relative overflow-hidden after:content-[""] after:absolute after:h-full after:w-full after:left-0 after:bottom-[-100%] after:bg-black after:rounded-full after:duration-150 after:ease-in-out after:hover:bottom-0 after:hover:rounded-none group'><p className='text-black z-50 relative duration-150 ease-in-out group-hover:text-white'>Register</p></NavLink>
             </div>
+            <button className={`px-1 py-1 rounded-md border-2 border-gray-300 cursor-pointer mt-5 ${toggleMenu ? 'hidden max-[900px]:block' : 'hidden'}`} onClick={handleToggle}><RxCross2 className='text-4xl' /></button>
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,75 +16,75 @@ function Home() {
   return (
     <div className='w-full flex items-center justify-center flex-col'>
       <div className='w-full h-screen flex items-center justify-center flex-col relative bg-gray-400/15 backdrop-blur-sm overflow-hidden rounded-b-2xl'>
-        <h1 className='text-black font-black font-merriwhether text-[5vw] text-center drop-shadow-lg leading-10 z-30 mt-14'><span className='text-black'>Inspiration at</span> Every Click</h1>
-        <h1 className='text-black font-black font-merriwhether text-[5vw] text-center drop-shadow-lg z-30 mt-6'>Explore, Learn, Grow.</h1>
-        <img src={fitnessPic} alt="fitness pic" className='w-40 h-60 object-center object-cover rounded-sm absolute top-[15%] left-[3%] rotate-12' />
-        <img src={travelPic} alt="travel pic" className='w-44 h-60 object-center object-cover rounded-sm absolute top-[16%] right-[2.5%] rotate-[-15deg]' />
-        <img src={technologyPic} alt="technology pic" className='w-44 h-60 object-center object-cover rounded-sm absolute bottom-[6%] right-[3%] rotate-12' />
-        <img src={foodPic} alt="food pic" className='w-44 h-60 object-center object-cover rounded-sm absolute bottom-[6%] left-[3%] rotate-[-15deg]' />
+        <h1 className='text-black font-black font-merriwhether text-[5vw] text-center drop-shadow-lg leading-10 z-30 mt-14 max-sm:text-2xl'><span className='text-black'>Inspiration at</span> Every Click</h1>
+        <h1 className='text-black font-black font-merriwhether text-[5vw] text-center drop-shadow-lg z-30 mt-6 max-sm:text-2xl'>Explore, Learn, Grow.</h1>
+        <img src={fitnessPic} alt="fitness pic" className='w-40 h-60 object-center object-cover rounded-md absolute top-[15%] left-[3%] rotate-12 max-lg:h-56 max-lg:w-36 max-md:h-44 max-md:w-44 max-sm:size-32 max-sm:top-[20%]' />
+        <img src={travelPic} alt="travel pic" className='w-44 h-60 object-center object-cover rounded-md absolute top-[16%] right-[3%] rotate-[-15deg] max-lg:h-56 max-lg:w-36 max-md:h-44 max-md:w-44 max-sm:size-32 max-sm:top-[20%]' />
+        <img src={technologyPic} alt="technology pic" className='w-44 h-60 object-center object-cover rounded-md absolute bottom-[6%] right-[3%] rotate-12 max-lg:h-56 max-lg:w-36 max-md:h-44 max-md:w-44 max-sm:size-32 max-sm:bottom-[10%]' />
+        <img src={foodPic} alt="food pic" className='w-44 h-60 object-center object-cover rounded-md absolute bottom-[6%] left-[3%] rotate-[-15deg] max-lg:h-56 max-lg:w-36 max-md:h-44 max-md:w-44 max-sm:size-32 max-sm:bottom-[10%]' />
       </div>
       <div className='w-full p-5 flex flex-col items-center justify-center bg-slate-400/25 backdrop-blur-sm'>
-        <div className='w-full p-2 h-screen flex items-center justify-center gap-20'>
-          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight'>Craft your digital identity - Build your blogger profile today</h1>
-          <div className='size-80 rounded-full overflow-hidden'>
+        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 max-[768px]:flex-col'>
+          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight max-[768px]:text-2xl'>Craft your digital identity - Build your blogger profile today</h1>
+          <div className='size-80 rounded-full overflow-hidden max-[768px]:w-56 max-[768px]:h-56'>
             <img src={hapinessPic} alt="hapiness pic" className='w-full h-full object-top object-cover' />
           </div>
         </div>
       </div>
       <div className='w-full p-5 flex flex-col items-center justify-center bg-cyan-400/25 backdrop-blur-sm'>
-        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 flex-row-reverse'>
-          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight'>Write, design, publish - Bring your blog to life effortlessly!</h1>
-          <div className='size-80 rounded-full overflow-hidden'>
+        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 flex-row-reverse max-[768px]:flex-col'>
+          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight max-[768px]:text-2xl'>Write, design, publish - Bring your blog to life effortlessly!</h1>
+          <div className='size-80 rounded-full overflow-hidden max-[768px]:w-56 max-[768px]:h-56'>
             <img src={publishPic} alt="hapiness pic" className='w-full h-full object-top object-cover' />
           </div>
         </div>
       </div>
       <div className='w-full p-5 flex flex-col items-center justify-center bg-lime-400/25 backdrop-blur-sm'>
-        <div className='w-full p-2 h-screen flex items-center justify-center gap-20'>
-          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight'>Craft your digital identity - Build your blogger profile today</h1>
-          <div className='size-80 rounded-full overflow-hidden'>
+        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 max-[768px]:flex-col'>
+          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight max-[768px]:text-2xl'>Craft your digital identity - Build your blogger profile today</h1>
+          <div className='size-80 rounded-full overflow-hidden max-[768px]:w-56 max-[768px]:h-56'>
             <img src={engagingPic} alt="hapiness pic" className='w-full h-full object-center object-cover' />
           </div>
         </div>
       </div>
       <div className='w-full p-5 flex flex-col items-center justify-center bg-amber-400/25 backdrop-blur-sm'>
-        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 flex-row-reverse'>
-          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight'>Spread the love - Like and share your favorite blog posts with the world!</h1>
-          <div className='size-80 rounded-full overflow-hidden'>
+        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 flex-row-reverse max-[768px]:flex-col'>
+          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight max-[768px]:text-2xl'>Spread the love - Like and share your favorite blog posts with the world!</h1>
+          <div className='size-80 rounded-full overflow-hidden max-[768px]:w-56 max-[768px]:h-56'>
             <img src={sharePic} alt="hapiness pic" className='w-full h-full object-top object-cover' />
           </div>
         </div>
       </div>
       <div className='w-full p-5 flex flex-col items-center justify-center bg-emerald-400/25 backdrop-blur-sm'>
-        <div className='w-full p-2 h-screen flex items-center justify-center gap-20'>
-          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight'>Organize your thoughts - Tag and categorize your posts for easy discovery!</h1>
-          <div className='size-80 rounded-full overflow-hidden'>
+        <div className='w-full p-2 h-screen flex items-center justify-center gap-20 max-[768px]:flex-col'>
+          <h1 className='font-bold font-merriwhether text-[3.5vw] text-center max-w-3xl text-slate-950 leading-tight max-[768px]:text-2xl'>Organize your thoughts - Tag and categorize your posts for easy discovery!</h1>
+          <div className='size-80 rounded-full overflow-hidden max-[768px]:w-56 max-[768px]:h-56'>
             <img src={ideaPic} alt="hapiness pic" className='w-full h-full object-left object-cover' />
           </div>
         </div>
       </div>
       <div className='w-full p-5 flex items-center justify-center flex-col'>
-        <h3 className='text-5xl font-playfair font-bold text-center mt-10 text-slate-950 capitalize'>So what are you waiting for</h3>
-        <h3 className='text-5xl font-playfair font-bold text-center mt-4 text-slate-950 capitalize'>Join now to explore </h3>
-        <h3 className='text-5xl font-playfair font-bold text-center mt-4 text-slate-950 capitalize'>the world with different perspective</h3>
-        <Link to={"/login"} className='text-xl font-noto font-medium cursor-pointer px-10 py-5 border border-black rounded-full relative overflow-hidden after:content-[""] after:absolute after:h-full after:w-full after:left-[-100%] after:bottom-0 after:bg-black after:rounded-full after:duration-150 after:ease-in-out after:hover:left-0 after:hover:rounded-none group mt-10'><p className='text-black z-50 relative duration-150 ease-in-out group-hover:text-white'>Get Started</p></Link>
+        <h3 className='text-5xl font-playfair font-bold text-center mt-10 text-slate-950 capitalize max-[768px]:text-xl max-[768px]:mt-2'>So what are you waiting for</h3>
+        <h3 className='text-5xl font-playfair font-bold text-center mt-4 text-slate-950 capitalize max-[768px]:text-xl max-[768px]:mt-2'>Join now to explore </h3>
+        <h3 className='text-5xl font-playfair font-bold text-center mt-4 text-slate-950 capitalize max-[768px]:text-xl max-[768px]:mt-2'>the world with different perspective</h3>
+        <Link to={"/login"} className='text-xl font-noto font-medium cursor-pointer px-10 py-5 border border-black rounded-full relative overflow-hidden after:content-[""] after:absolute after:h-full after:w-full after:left-[-100%] after:bottom-0 after:bg-black after:rounded-full after:duration-150 after:ease-in-out after:hover:left-0 after:hover:rounded-none group mt-10 max-[768px]:px-6 max-[768px]:py-3'><p className='text-black z-50 relative duration-150 ease-in-out group-hover:text-white'>Get Started</p></Link>
       </div>
-      <footer class="bg-slate-950 w-full h-24">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center font-noto">© 2023 <a href="https://flowbite.com/" class="hover:underline">Blog™</a>. All Rights Reserved.
+      <footer className="bg-slate-950 w-full p-2">
+        <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between max-md:flex-col">
+          <span className="text-sm text-gray-500 sm:text-center font-noto">© 2023 <a href="https://flowbite.com/" className="hover:underline">Blog™</a>. All Rights Reserved.
           </span>
-          <ul class="flex flex-wrap items-center mt-3 text-lg font-medium text-gray-500 sm:mt-0 font-noto capitalize">
+          <ul className="flex flex-wrap items-center mt-3 text-lg font-medium text-gray-500 sm:mt-0 font-noto capitalize max-md:flex-col">
             <li>
-              <Link class="hover:underline me-4 md:me-6">About</Link>
+              <Link className="hover:underline me-4 md:me-6">About</Link>
             </li>
             <li>
-              <Link class="hover:underline me-4 md:me-6">Contact us</Link>
+              <Link className="hover:underline me-4 md:me-6">Contact us</Link>
             </li>
             <li>
-              <Link class="hover:underline me-4 md:me-6">Privacy policy</Link>
+              <Link className="hover:underline me-4 md:me-6">Privacy policy</Link>
             </li>
             <li>
-              <Link class="hover:underline me-4 md:me-6">Licensing</Link>
+              <Link className="hover:underline me-4 md:me-6">Licensing</Link>
             </li>
           </ul>
         </div>
